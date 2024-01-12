3769f4aad861774158fb74d82ea3254da2e2f2f3
gleicesantosdev <116775112+gleicesantosdev@users.noreply.github.com>
2024-01-12 16:03:52 -0300

    Class pacientes

--- Pacientes.java
+++ Pacientes.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Pacientes {
-    
+    String Nome;
+    String Telefone;
+
+    public Pacientes(String nome, String telefone) {
+        this.Nome= nome;
+        this.Telefone = telefone;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        Telefone = telefone;
+    }
 }
